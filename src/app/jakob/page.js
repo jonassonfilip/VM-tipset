79e3d8356cb32f2d15594d26d0b0c273f8f63c5e
Filip Jonasson <jonassonfilip@gmail.com>
2026-06-17 01:25:11 +0200

    Adding back button on coupon page

--- src/app/jakob/page.js
+++ src/app/jakob/page.js
@@ -1,10 +1,18 @@
 import Image from 'next/image';
 import coupon from '/public/jakob-1.png';
 import styles from '../page.module.css';
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <div className={styles.coupon}>
+      <Link
+        href="/"
+        className={styles.backButton}
+        aria-label="Tillbaka till startsidan"
+      >
+        ←
+      </Link>
       <h1>Jakob Gometz</h1>
       <Image src={coupon} alt="The participants magnificent bet" />
     </div>
--- src/app/niklas/page.js
+++ src/app/niklas/page.js
@@ -1,10 +1,18 @@
 import Image from 'next/image';
 import coupon from '/public/niklas-1.png';
 import styles from '../page.module.css';
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <div className={styles.coupon}>
+      <Link
+        href="/"
+        className={styles.backButton}
+        aria-label="Tillbaka till startsidan"
+      >
+        ←
+      </Link>
       <h1>Niklas Andersson</h1>
       <Image src={coupon} alt="The participants magnificent bet" />
     </div>

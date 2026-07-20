fd89b3f81fb11f1b91f91fe2a8d8e17f91aaf403
Filip Jonasson <jonassonfilip@gmail.com>
2026-07-20 12:55:44 +0200

    Final table and coupons

--- src/app/page.js
+++ src/app/page.js
@@ -82,172 +82,188 @@ export default function Home() {
           </div>
         </div>
         <div className={styles.table}>
-          <h2>Tabell (8/7 efter åttondelarna):</h2>
+          <h2>Slutgiltig tabell VM-tipset 2026:</h2>
           <div className={styles.tableRows}>
             <div className={styles.tableRow}>
-              <h4>460 poäng:</h4>
+              <h3 className={styles.gold}>535 poäng: (Mästaren: 5000 kr)</h3>
               <ul>
-                <li>Joakim</li>
+                <li>
+                  <h3>Joakim</h3>
+                </li>
               </ul>
 
-              <h4>455 poäng:</h4>
+              <h4 className={styles.silver}>530 poäng: (2:a plats: 2000 kr)</h4>
               <ul>
-                <li>Alexander</li>
                 <li>Robin</li>
               </ul>
 
-              <h4>440 poäng:</h4>
-              <ul>
-                <li>Andreas</li>
-              </ul>
-
-              <h4>435 poäng:</h4>
+              <h4 className={styles.bronze}>510 poäng: (3:e plats: 1200 kr)</h4>
               <ul>
                 <li>Johan M</li>
               </ul>
 
-              <h4>430 poäng:</h4>
+              <h4>500 poäng: (4:e plats: 800 kr)</h4>
               <ul>
-                <li>Johan J</li>
+                <li>Alexander</li>
               </ul>
 
-              <h4>425 poäng:</h4>
+              <h4>485 poäng: (5:e & 6:e plats: 400+200 kr att dela på)</h4>
               <ul>
-                <li>Johan N</li>
+                <li>Linus</li>
+                <li>Noel</li>
               </ul>
 
-              <h4>420 poäng:</h4>
+              <h4>480 poäng:</h4>
               <ul>
                 <li>Lina</li>
               </ul>
 
-              <h4>415 poäng:</h4>
+              <h4>475 poäng:</h4>
               <ul>
                 <li>Filip</li>
               </ul>
 
-              <h4>410 poäng:</h4>
+              <h4>470 poäng:</h4>
               <ul>
+                <li>Andreas</li>
                 <li>Frida</li>
-                <li>Linus</li>
-                <li>Mattias</li>
-                <li>Noel</li>
+                <li>Johan N</li>
               </ul>
 
-              <h4>405 poäng:</h4>
-              <ul>
-                <li>Petra</li>
-              </ul>
-
-              <h4>395 poäng:</h4>
-              <ul>
-                <li>Henrik</li>
-                <li>Therese</li>
-              </ul>
-
-              <h4>390 poäng:</h4>
+              <h4>465 poäng:</h4>
               <ul>
                 <li>Gabriel</li>
+              </ul>
+
+              <h4>455 poäng:</h4>
+              <ul>
+                <li>Mattias</li>
+              </ul>
+
+              <h4>445 poäng:</h4>
+              <ul>
+                <li>Edvin</li>
+                <li>Johan J</li>
+                <li>Marcus</li>
+              </ul>
+
+              <h4>440 poäng:</h4>
+              <ul>
+                <li>Anders</li>
+                <li>Anncatrine</li>
+              </ul>
+
+              <h4>435 poäng:</h4>
+              <ul>
+                <li>Petra</li>
                 <li>Sam</li>
               </ul>
 
-              <h4>385 poäng:</h4>
+              <h4>430 poäng:</h4>
               <ul>
                 <li>Aksel</li>
                 <li>Clas</li>
-                <li>Edvin</li>
-                <li>Marcus</li>
-                <li>Niklas</li>
-                <li>Sofie</li>
-                <li>Sonja</li>
-              </ul>
-
-              <h4>380 poäng:</h4>
-              <ul>
-                <li>Alessandro</li>
+                <li>Martin</li>
               </ul>
             </div>
 
             <div className={styles.tableRow}></div>
 
             <div className={styles.tableRow}>
-              <h4>375 poäng:</h4>
+              <h4>425 poäng: (Mittenfållan: 300 kr efter lottning)</h4>
+              <ul>
+                <li>Alessandro</li>
+                <li>Oliver</li>
+                <li>
+                  <u>Therese</u>
+                </li>
+              </ul>
+
+              <h4>420 poäng:</h4>
+              <ul>
+                <li>Ludwig R</li>
+              </ul>
+
+              <h4>415 poäng:</h4>
+              <ul>
+                <li>Niklas</li>
+                <li>Sofie</li>
+                <li>Sonja</li>
+              </ul>
+
+              <h4>410 poäng:</h4>
+              <ul>
+                <li>Henrik</li>
+              </ul>
+
+              <h4>405 poäng:</h4>
+              <ul>
+                <li>Linn</li>
+                <li>Maria</li>
+              </ul>
+
+              <h4>390 poäng:</h4>
               <ul>
                 <li>Bodil</li>
+              </ul>
+
+              <h4>385 poäng:</h4>
+              <ul>
+                <li>Jakob</li>
+              </ul>
+
+              <h4>380 poäng:</h4>
+              <ul>
+                <li>Maya</li>
+                <li>Oskar</li>
+              </ul>
+
+              <h4>375 poäng:</h4>
+              <ul>
                 <li>Johannes</li>
-                <li>Ludwig R</li>
+                <li>Jonas</li>
               </ul>
 
               <h4>370 poäng:</h4>
               <ul>
                 <li>Hannes</li>
-                <li>Jakob</li>
-                <li>Martin</li>
               </ul>
 
               <h4>365 poäng:</h4>
               <ul>
-                <li>Anders</li>
-                <li>Anncatrine</li>
-              </ul>
-
-              <h4>360 poäng:</h4>
-              <ul>
-                <li>Maria</li>
+                <li>Johanna</li>
               </ul>
 
               <h4>355 poäng:</h4>
               <ul>
                 <li>Berit</li>
+                <li>Ludwig P</li>
               </ul>
 
               <h4>350 poäng:</h4>
               <ul>
-                <li>Johanna</li>
-                <li>Oliver</li>
-                <li>Oskar</li>
+                <li>Basta</li>
               </ul>
 
               <h4>345 poäng:</h4>
               <ul>
-                <li>Linn</li>
+                <li>Caroline</li>
+                <li>Joel</li>
               </ul>
 
               <h4>340 poäng:</h4>
               <ul>
-                <li>Ludwig P</li>
+                <li>Birgitta</li>
               </ul>
 
               <h4>335 poäng:</h4>
               <ul>
                 <li>Julian</li>
-                <li>Maya</li>
               </ul>
 
-              <h4>330 poäng:</h4>
-              <ul>
-                <li>Caroline</li>
-              </ul>
-
-              <h4>325 poäng:</h4>
-              <ul>
-                <li>Basta</li>
-                <li>Birgitta</li>
-              </ul>
-
-              <h4>315 poäng:</h4>
-              <ul>
-                <li>Joel</li>
-                <li>Jonas</li>
-              </ul>
-
-              <h4>305 poäng:</h4>
+              <h4>305 poäng: (Plåster på såren: 100 kr att dela på)</h4>
               <ul>
                 <li>Atti</li>
-              </ul>
-
-              <h4>290 poäng:</h4>
-              <ul>
                 <li>Peter</li>
               </ul>
             </div>
